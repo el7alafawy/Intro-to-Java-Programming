7f19d0187839633bdb287b34e9935ba568dd0d02
el7alafawy <66574111+el7alafawy@users.noreply.github.com>
2021-06-22 01:55:59 +0200

    Update Exercise_03_19.java
    The checking method should be changed due to Logical-mathematical error.

--- Exercise_03/Exercise_03_19/Exercise_03_19.java
+++ Exercise_03/Exercise_03_19/Exercise_03_19.java
@@ -22,21 +22,31 @@ public class Exercise_03_19 {
 		double x3 = input.nextDouble();
 		double y3 = input.nextDouble();
 
-		// Test if input is valid
-		boolean valid = (x1 + y1 > x3 + y3 && x2 + y2 > x3 + y3) ||
+		// Test if input is valid ( this check is WRONG, try (1,1) , (2,2) , (3,3) which can never form a triangle and it will tell you it's valid)
+/*		boolean valid = (x1 + y1 > x3 + y3 && x2 + y2 > x3 + y3) ||
 							 (x1 + y1 > x2 + y2 && x3 + y3 > x2 + y2) ||
 							 (x3 + y3 > x1 + y1 && x2 + y2 > x1 + y1);
-
+	 
 		if (!valid)
 		{
 			System.out.println("Input is invalid.");
 			System.exit(1);
 		}
+*/
 
 		// Compute the sides of the triangle
 		double side1 = Math.pow(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2), 0.5);
 		double side2 = Math.pow(Math.pow(x3 - x2, 2) + Math.pow(y3 - y2, 2), 0.5);
 		double side3 = Math.pow(Math.pow(x1 - x3, 2) + Math.pow(y1 - y3, 2), 0.5);
+		
+		// The correct check should be here
+		boolean valid= (side1 + side2 > side3)&&(side1+side3 >side2)&&(side2 + side3>side1);
+		
+		if (!valid)
+		{
+			System.out.println("Input is invalid.");
+			System.exit(1);
+		}
 
 		// Display the perimeter of the triangle
 		System.out.println("perimeter of triangle is " + (side1 + side2 + side3));
